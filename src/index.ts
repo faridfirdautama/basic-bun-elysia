cd633d1411d04549b369ff6ae41dfe290e75ffce
Dadit <dadit.utama@gmail.com>
2024-08-09 06:16:05 +0700

    feat: basic route

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .get("/api/v1/users", () => "Hi Users")
+  .get("/api/v1/products", () => "Hi Products")
+  .listen(3000);
+
+app.handle(new Request("http://localhost:3000/")).then(console.log());
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
